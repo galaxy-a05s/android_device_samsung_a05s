9d0a2b809687299f060b3bd13e4cc98ba7062a3a
SavedByLight <mrfluffyoven@gmail.com>
2025-01-31 11:40:18 +0000

    import S3 kernel & modules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,10 @@ TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 
+# Modules
+TW_LOAD_VENDOR_MODULES := true
+TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/lib/modules)\")
+
 # Inherit from common tree
 include device/samsung/a05s-common/BoardConfigCommon.mk
 
